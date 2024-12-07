decb2b70a10c0c1deab7a1cae105e1ef96bc9a4b
harshithaalasakani <harshithaalasakani@gmail.com>
2024-12-07 19:55:15 +0530

    project commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText; 
+    private float score;
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            score += 1 * Time.deltaTime;
+            scoreText.text = ((int)score).ToString();
+        }
+    }
+}
